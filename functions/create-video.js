c0ad8be5e5fafbf871481280ea3d41fd845a4ec5
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-09-24 09:59:03 -0700

    add cors support

--- functions/create-video.js
+++ functions/create-video.js
@@ -4,6 +4,12 @@ exports.handler = async (event, context) => {
   console.log(manifest)
   return {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({
       manifest: manifest
     }),
